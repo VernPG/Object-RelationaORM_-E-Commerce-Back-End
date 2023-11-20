a8385dc078092a7b30516f1ed5da91924ee77c56
Veronica Perez <veperez@live.com>
2023-11-20 16:09:30 -0600

    update

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 // import models
-const Product = require('./product');
-const Category = require('./category');
-const Tag = require('./tag');
-const ProductTag = require('./productTag');
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
